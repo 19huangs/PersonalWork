f5819f2c7c00a06b168c61377c825b3902184b00
Sabrina Huang <19huangs@elmbrookstudents.org>
2018-09-13 13:52:55 -0500

    Mailing Label

---
+++ src/outputExamples/MailingLabel.java
@@ -0,0 +1,10 @@
+package outputExamples;
+
+public class MailingLabel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("    Sabrina Huang \n 18285 Peregrine Lane \n Brookfield, WI 53045");
+	}
+
+}
