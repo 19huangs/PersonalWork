aa7e9c28262e34766a7a8e5d031926bc403ca30f
Sabrina Huang <19huangs@elmbrookstudents.org>
2018-09-13 13:10:07 -0500

    Name on Two Lines

---
+++ src/outputExamples/NameOnTwoLines.java
@@ -0,0 +1,11 @@
+package outputExamples;
+
+public class NameOnTwoLines {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Sabrina"); 
+System.out.println("Huang");
+	}
+
+}
