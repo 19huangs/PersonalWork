5f6d255c5ce9233356482d0c8f1fc9f398fe6638
Sabrina Huang <19huangs@elmbrookstudents.org>
2018-09-13 13:17:40 -0500

    FirstNameLastNameAge

---
+++ src/outputExamples/FirstNameLastNameAge.java
@@ -0,0 +1,12 @@
+package outputExamples;
+
+public class FirstNameLastNameAge {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Sabrina");
+		System.out.println("Huang");
+		System.out.println("17");
+	}
+
+}
