2a8d8dc896a4764c5ec10ef12130e5c24fead11f
Sabrina Huang <19huangs@elmbrookstudents.org>
2018-09-21 14:26:46 -0500

    Age in 7 Years (Other Way)

--- src/mathOperators/Age.java
+++ src/mathOperators/Age.java
@@ -5,11 +5,12 @@ public class Age {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int age = -999;
+		int future = -999;
 		Scanner input= new Scanner(System.in);
 		System.out.println("How old are you?");
 		age= input.nextInt();
-		
-		System.out.println("In 7 years, you will be " + (age +7) + " years old.");
+		future = age + 7;
+		System.out.println("In 7 years, you will be " + (future) + " years old.");
 		
 	}
 
