3b26dc4d811cfc57e5445503081d613b548e4dfd
Sabrina Huang <19huangs@elmbrookstudents.org>
2018-09-24 14:47:26 -0500

    Sphere Update

--- src/mathOperators/Sphere.java
+++ src/mathOperators/Sphere.java
@@ -6,11 +6,13 @@ public class Sphere {
 		// TODO Auto-generated method stub
 		double radius = -999.99;
 		double sphere = -999.99;
-		
 		Scanner input = new Scanner(System.in);
+		
 		System.out.println("What is the radius of the sphere?");
+		
 		radius = input.nextDouble();
 		sphere = (radius * radius * radius) * (Math.PI) * (4.0/3.0);
+		
 		System.out.println("The volume of the sphere is " + sphere);
 	}
 
