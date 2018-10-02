f4ba09dbcb13ffbbd44acbe8f5abb196b22051dc
Sabrina Huang <19huangs@elmbrookstudents.org>
2018-10-02 13:50:43 -0500

    Bob Hello

---
+++ src/objectOriented/GreeterTestClass.java
@@ -0,0 +1,11 @@
+package objectOriented;
+
+public class GreeterTestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+greeter bob = new greeter();
+bob.sayHello();
+	}
+
+}
