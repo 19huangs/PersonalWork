406870c50f9d109e22514e709d123f3663ed677a
Sabrina Huang <19huangs@elmbrookstudents.org>
2018-10-02 14:13:38 -0500

    Neighbor Test Class

---
+++ src/objectOriented/NeighborTestClass.java
@@ -0,0 +1,13 @@
+package objectOriented;
+
+public class NeighborTestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Neighbor aafreen = new Neighbor();
+		aafreen.sayJoke();
+		aafreen.sayMoney();
+		aafreen.sayConversation();
+	}
+
+}
