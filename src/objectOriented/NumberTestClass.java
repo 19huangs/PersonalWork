de0d213ff04ad4777b8f490d1a48176b8ae743a5
Sabrina Huang <19huangs@elmbrookstudents.org>
2018-10-05 15:04:04 -0500

    Return Values

--- src/objectOriented/NumberTestClass.java
+++ src/objectOriented/NumberTestClass.java
@@ -8,6 +8,15 @@ public class NumberTestClass {
 		bob.sayNumber(7.5);
 		bob.sayNumberPlus2(9.8);
 		bob.saySum(5.0, 10.1);
+		
+		double square = bob.returnSquare(5.0);
+		System.out.println("Your number is: " + square);
+		
+		double area = bob.returnArea(5.0,10.0);
+		System.out.println("Your area is: " + area);
+		
+		int num2 = bob.returnRoundUp(5.3);
+		System.out.println("Your number rounded up is: " + num2);
 	}
 
 }
--- src/objectOriented/Numbers.java
+++ src/objectOriented/Numbers.java
@@ -7,12 +7,30 @@ public class Numbers {
 	public void sayNumber(double num) {
 		System.out.println("Your number is: " + num);
 	}
-	public void sayNumberPlus2(double num){
+
+	public void sayNumberPlus2(double num) {
 		num = num + 2;
 		System.out.println("Your number plus 2 is: " + num);
 	}
-	public void saySum (double num, double num2){
+
+	public void saySum(double num, double num2) {
 		num = num + num2;
 		System.out.println("The sum is: " + num);
+
+	}
+
+	public double returnSquare(double square) {
+		square = square * square;
+		return square;
+	}
+
+	public double returnArea(double length, double width) {
+		double area = length * width;
+		return area;
+	}
+	
+	public int returnRoundUp(double num) {
+		int num2 =(int) Math.ceil(num);
+		return num2;
 	}
 }
