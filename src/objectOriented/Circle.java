81abbde27487fdcb1b89a6bcf2f24a438913065a
Unknown <19huangs@elmbrookstudents.org>
2018-10-09 20:46:45 -0500

    Circle Accessors and Mutators

---
+++ src/objectOriented/Circle.java
@@ -0,0 +1,26 @@
+package objectOriented;
+
+public class Circle {
+	private double radius;
+
+	public Circle() {
+		radius = -999.99;
+
+	}
+
+	public double returnRadius() {
+		return radius;
+	}
+
+	public double Circumfrence(double circumfrence) {
+		return circumfrence;
+	}
+
+	public double Area(double area) {
+		return area;
+	}
+
+	public void changeRadius(double xRadius) {
+		radius = xRadius;
+	}
+}
