480a9560f5ba4fe56ae371ebae28d4719b34ff7c
Sabrina Huang <19huangs@elmbrookstudents.org>
2018-10-18 13:29:43 -0500

    Car Insurance Decision

---
+++ src/decisions/CarInsurance.java
@@ -0,0 +1,40 @@
+package decisions;
+import java.util.Scanner;
+public class CarInsurance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner (System.in);
+		double cost = 200.0;
+		System.out.println("What is your age?");
+		double age = input. nextDouble();
+		System.out.println("What is your weight?");
+		double weight = input. nextDouble();
+		System.out.println("Do you smoke?");
+		boolean smoke = input. nextBoolean();
+		
+		if (age>75.0)
+		{
+			cost += 125;
+		}
+		
+		els (age< 30.0)
+		{
+			cost -= 25;
+		}
+		
+		if (weight> 700.0)
+		{
+			cost += 300;
+		}
+		
+		if (smoke== true)
+		{
+			cost += 50;
+		}
+			cost-=40;
+			
+		System.out.println("Cost: " + cost);	
+	}
+
+}
