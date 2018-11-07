c74a79429bc87fcb3d803e0f07c96691318f795f
Sabrina Huang <19huangs@elmbrookstudents.org>
2018-11-07 14:21:37 -0600

    Update Random Numbers

--- src/randomNumbers/GoogleDriveProblems.java
+++ src/randomNumbers/GoogleDriveProblems.java
@@ -5,7 +5,7 @@ public class GoogleDriveProblems {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Random aafreen = new Random ();
+		Random aafreen = new Random (1);
 		Scanner input = new Scanner (System.in);
 		
 		
@@ -30,7 +30,7 @@ public class GoogleDriveProblems {
 	}*/
 		
 //ROLL A DIE 2
-		double rolls =0;
+		/*double rolls =0;
 		System.out.println("How many rolls?");
 		rolls = input.nextInt();
 		
@@ -80,5 +80,72 @@ public class GoogleDriveProblems {
 		System.out.println("Rolled a four percentage: " + ((four/rolls) *100));
 		System.out.println("Rolled a five percentage: " + ((five/rolls) *100));
 		System.out.println("Rolled a six percentage: " + ((six/rolls) *100));
+	}	*/
+
+		
+//FLIP A COIN
+	/*	int counter1 = 0;
+		int counter2 = 0;
+		int min = 1;
+		int max = 2;
+		int randomNum = 0;
+		int heads = 0;
+		int tails = 0;
+		double largest = 0;
+		double smallest = 100000;
+		for (counter1 =1; counter1 <=1000000; counter1++)
+		{
+			for (counter2 =1; counter2 <=100000; counter2++)
+			{
+				randomNum = min + aafreen.nextInt(max-min+1);
+				if (randomNum == 1)
+				{
+					heads++;
+				}
+				
+			}
+			if (heads>= largest)
+			{
+				largest = heads;
+			
+
+			}
+			
+			if (heads<= smallest)
+			{
+				smallest = heads;
+			}
+			heads = 0;
+		}
+		System.out.println("Maximum percentage: " + ((largest * 100)/100000) + "%");
+		System.out.println("Minimum percentage: " + ((smallest * 100)/100000) + "%");
+	} */
+		
+//YAHTZEE
+		int min = 1;
+		int max = 6;
+		int randomNum1 = 0;
+		int randomNum2 = 0;
+		int randomNum3 = 0;
+		int randomNum4 = 0;
+		int randomNum5 = 0;
+		int rolls = 0;
+		int yes =0;
+		
+		do 
+		{ 	rolls++;
+			randomNum1 = min + aafreen.nextInt(max-min+1);
+			randomNum2 = min + aafreen.nextInt(max-min+1);
+			randomNum3 = min + aafreen.nextInt(max-min+1);
+			randomNum4 = min + aafreen.nextInt(max-min+1);
+			randomNum5 = min + aafreen.nextInt(max-min+1);
+		if (randomNum1 == randomNum2 && randomNum1 == randomNum3 && randomNum1 == randomNum4 && randomNum1 == randomNum5)
+		{
+			System.out.println("You got Yahtzee!");
+			yes = 1;
+		}
+		}
+		while (yes!=1);
+		System.out.println("Number of rolls: " + rolls);
 	}	
 }
