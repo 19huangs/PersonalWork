515bb9819ddc56bb80fccb93680717271c960057
Sabrina Huang <19huangs@elmbrookstudents.org>
2018-11-26 14:59:03 -0600

    arrays

---
+++ src/arrays/ClassActivity.java
@@ -0,0 +1,74 @@
+package arrays;
+import java.util.Scanner;
+import java.util.Random;
+public class ClassActivity {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner (System.in);
+		Random bob = new Random ();
+//Store 5 values	
+		/*int [] testscores = new int [5];
+		
+		for (int counter = 0; counter <=4; counter++)
+		{
+			System.out.println("Give A Score: ");
+			testscores [counter] = input.nextInt();
+		}
+		System.out.println(testscores[1]);*/
+
+//Sum of array values	
+		/*int [] testscores = new int [5];
+		
+		for (int counter = 0; counter <=4; counter++)
+		{
+			System.out.println("Give A Score: ");
+			testscores [counter] = input.nextInt();
+		}
+		int sum = 0; 
+		for (int counter = 0; counter <=4; counter++)
+		{
+			sum +=testscores [counter];
+		}
+		System.out.println("Sum: " + sum);
+	}*/
+		
+		
+//backward 		
+		
+		/*int [] testscores = new int [5];
+		
+		for (int counter = 1; counter <=5; counter++)
+		{
+			System.out.println("Give A Score: ");
+			testscores [counter] = input.nextInt();
+		}
+		
+		for (int counter = 4; counter >=0; counter--)
+		{
+			System.out.println(testscores[counter]);
+		}
+	}*/
+
+//random number
+		/*int largest = 0;
+		int min = 0; 
+		int max = 1000; 
+		int randomNum = 0;
+		int [] testscores = new int [250];
+		
+		for (int counter1 =1; counter1 <=250; counter1++) 
+		{ 
+			randomNum = min + bob.nextInt(max-min+1);
+			
+			if (randomNum > largest)
+			{
+				largest = randomNum;
+			}
+		}
+		
+		System.out.println(largest);
+	}*/
+		
+
+}
