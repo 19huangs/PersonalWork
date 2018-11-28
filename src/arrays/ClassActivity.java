e6e17fbd719abe6ad269832b87303e251fc407ff
Sabrina Huang <19huangs@elmbrookstudents.org>
2018-11-28 13:45:14 -0600

    Arrays updated

--- src/arrays/ClassActivity.java
+++ src/arrays/ClassActivity.java
@@ -70,5 +70,84 @@ public class ClassActivity {
 		System.out.println(largest);
 	}*/
 		
-
+	//Problem with testscores.length	
+		
+		/*final int numScores= 5;
+		int [] testscores = new int [numScores];
+		
+		for (int counter = 0; counter < testscores.length; counter++)
+		{
+			System.out.println("Give A Score: ");
+			testscores [counter] = input.nextInt();
+		}
+		int sum = 0; 
+		for (int counter = 0; counter < testscores.length; counter++)
+		{
+			sum +=testscores [counter];
+		}
+		System.out.println("Sum: " + sum);
+	}*/
+		
+//20 Names backward
+		/*final int numNames= 20;
+		String [] names = new String [numNames];
+		
+		for (int counter = 0; counter < names.length; counter++)
+		{
+			System.out.println("Give A Name: ");
+			names [counter] = input.nextLine();
+		}
+		
+		for (int counter2 = (names.length-1); counter2 >=0; counter2--)
+		{
+			System.out.println(names[counter2]);
+		}
+	}	*/
+	//last letter
+		/* final int numNames= 20;
+		String [] names = new String [numNames];
+		
+		for (int counter = 0; counter < names.length; counter++)
+		{
+			System.out.println("Give A Name: ");
+			names [counter] = input.nextLine();
+		}
+		
+		System.out.println("Give a letter: ");
+		String letter = input.nextLine();
+		
+		for (int counter = 0; counter < names.length; counter++)
+		{
+			char endletter = names[counter].charAt(names[counter].length()-1);
+			if (endletter == letter.charAt(0))
+			{
+				System.out.println(names[counter]);
+			}
+		}*/
+		
+		
+		
+		final int numNames= 20;
+		String [] names = new String [numNames];
+		
+		for (int counter = 0; counter < names.length; counter++)
+		{
+			System.out.println("Give A Name: ");
+			names [counter] = input.nextLine();
+		}
+		
+		System.out.println("Length of desired name: ");
+		int length = input.nextInt();
+		int amount = 0;
+		
+		for (int counter = 0; counter < names.length; counter++)
+		{
+			int fullLength = names[counter].length();
+			if (fullLength == length)
+			{
+				amount++;
+			}
+		}
+		System.out.println("Number of friends with " + length + " letters: " + amount);
+	}
 }
