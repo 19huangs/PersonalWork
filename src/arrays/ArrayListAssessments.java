5d51e0dc7f31a2e2078c8fa390745e1d6e91a8db
Sabrina Huang <19huangs@elmbrookstudents.org>
2018-12-07 15:03:06 -0600

    Array List day 3

---
+++ src/arrays/ArrayListAssessments.java
@@ -0,0 +1,45 @@
+package arrays;
+import java.util.Scanner;
+import java.util.Random;
+import java.util.ArrayList;
+public class ArrayListAssessments {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner (System.in);
+//Friend ArrayList
+		/*ArrayList <FriendClass> cat = new ArrayList<FriendClass>();
+		
+		for (int counter =0; counter<13; counter++)
+		{
+			System.out.println("Friend's Name: ");
+			String name = input.nextLine();
+			
+			System.out.println("Friend's Age: ");
+			int age = input.nextInt();
+			input.nextLine();
+			
+			cat.add(new FriendClass(name, age));
+			
+		}
+		System.out.println("Friends' Names in Reverse Order: ");
+		for (int counter2 =12; counter2>=0; counter2--)
+		{
+			
+			System.out.println(cat.get(counter2).getName());
+		}
+		
+		System.out.println("Years until each Friend can retire in order: ");
+		for (int counter =0; counter<13; counter++)
+		{
+			System.out.println(65-cat.get(counter).getAge() + " years");
+		}
+	}*/
+		
+//DOG
+		
+		
+		
+		
+	}
+}
